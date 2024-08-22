659b0d7f17499ed323466f85fea928f75875af26
Amilcarqs <160414334+Amilcarqs@users.noreply.github.com>
2024-08-22 15:51:50 -0400

    Add files via upload

---
+++ Tarea1(htmlAPhp)listrace/index.php
@@ -0,0 +1,9 @@
+<?php include 'cabecera.inc.php' ?>
+<h2>SITIO CON PHP <br> PROGRAMACION WEB 3 </h2>
+<?php include 'bienvenida.inc.php' ?>
+<br>	
+<?php include 'botones.inc.php' ?>
+<?php include 'botones.inc.php' ?>
+<?php include 'botones.inc.php' ?>
+<?php include 'botones.inc.php' ?>
+<?php include 'pie.inc.php' ?>
